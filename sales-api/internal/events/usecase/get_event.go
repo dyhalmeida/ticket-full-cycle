a391b2c8fbe85c33c635dcc884af88d4b9980864
dyhalmeida <dyhalmeida@gmail.com>
2024-07-07 09:34:31 -0300

    feat(sales-api): add get event usecase

---
+++ sales-api/internal/events/usecase/get_event.go
@@ -0,0 +1,46 @@
+package usecase
+
+import "github.com/dyhalmeida/ticket-full-cycle/sales-api/internal/events/domain"
+
+type GetEventInputDTO struct {
+	ID string
+}
+
+type GetEventOutputDTO struct {
+	ID           string  `json:"id"`
+	Name         string  `json:"name"`
+	Location     string  `json:"location"`
+	Organization string  `json:"organization"`
+	Rating       string  `json:"rating"`
+	Date         string  `json:"date"`
+	Capacity     int     `json:"capacity"`
+	Price        float64 `json:"price"`
+	PartnerID    int     `json:"partner_id"`
+}
+
+type GetEventUseCase struct {
+	eventRepository domain.EventRepositoryInterface
+}
+
+func NewGetEventUseCase(eventRepository domain.EventRepositoryInterface) *GetEventUseCase {
+	return &GetEventUseCase{eventRepository: eventRepository}
+}
+
+func (uc *GetEventUseCase) Execute(input GetEventInputDTO) (*GetEventOutputDTO, error) {
+	event, err := uc.eventRepository.FindEventByID(input.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	return &GetEventOutputDTO{
+		ID:           event.ID,
+		Name:         event.Name,
+		Location:     event.Location,
+		Organization: event.Organization,
+		Rating:       string(event.Rating),
+		Date:         event.Date.Format("2006-01-02 15:04:05"),
+		Capacity:     event.Capacity,
+		Price:        event.Price,
+		PartnerID:    event.PartnerID,
+	}, nil
+}
